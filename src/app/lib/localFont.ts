0dac4948e53c2d3d4b8f5eea3691869d75bd92f5
nosignificant <nosignificantment@gmail.com>
2025-12-30 02:06:13 +0900

    ㅇ

--- src/app/lib/localFont.ts
+++ src/app/lib/localFont.ts
@@ -2,7 +2,7 @@
 import localFont from "next/font/local";
 
 export const pretendard = localFont({
-  src: "../fonts/pretendardVariable.woff2",
+  src: "../fonts/PretendardVariable.woff2",
   variable: "--font-pretendard",
   weight: "100 900",
   style: "normal",
